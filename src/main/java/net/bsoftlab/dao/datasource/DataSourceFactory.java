b7e8c0380355cfcaec81db37dcb87459dae47a43
Ghukas Kakosyan <kakosyanghukas@gmail.com>
2019-03-07 13:00:48 +0400

    DataSourceFactory is changed.

--- src/main/java/net/bsoftlab/dao/datasource/DataSourceFactory.java
+++ src/main/java/net/bsoftlab/dao/datasource/DataSourceFactory.java
@@ -4,14 +4,18 @@ import javax.sql.DataSource;
 import com.mysql.cj.jdbc.MysqlDataSource;
 
 public class DataSourceFactory {
+    private static DataSource dataSource = null;
     public static DataSource getDataSource () {
-        String urlDatabaseMysql = "jdbc:mysql://localhost:3306/access";
-        String userNameDatabaseMysql = "root";
-        String passwordDatabaseMysql = "gktj19760207";
-        MysqlDataSource mysqlDataSource = new MysqlDataSource();
-        mysqlDataSource.setURL(urlDatabaseMysql);
-        mysqlDataSource.setUser(userNameDatabaseMysql);
-        mysqlDataSource.setPassword(passwordDatabaseMysql);
-        return mysqlDataSource;
+        if (dataSource == null) {
+            String urlDatabaseMysql = "jdbc:mysql://localhost:3306/access";
+            String userNameDatabaseMysql = "root";
+            String passwordDatabaseMysql = "gktj19760207";
+            MysqlDataSource mysqlDataSource = new MysqlDataSource();
+            mysqlDataSource.setURL(urlDatabaseMysql);
+            mysqlDataSource.setUser(userNameDatabaseMysql);
+            mysqlDataSource.setPassword(passwordDatabaseMysql);
+            dataSource = mysqlDataSource;
+        }
+        return dataSource;
     }
 }
